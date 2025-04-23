4b9dd217abb5e971f39404bfc9270540d98c5287
dinhnam811 <nguyendinhnam8a@gmail.com>
2025-04-23 18:15:26 +1000

    lc1399

--- Easy/lc1399_count_largest_group/Solution/Program.cs
+++ Easy/lc1399_count_largest_group/Solution/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using NUnit.Framework;
 public class Solution {
     public int DigitSum(int n) {
         int sum = 0;
@@ -12,7 +12,7 @@ public class Solution {
         Dictionary<int,int> myDic = new();
         for (int i = 0; i <= n; i++)
         {
-            key = DigitSum(i);
+            int key = DigitSum(i);
             if (!myDic.ContainsKey(key))
             {
                 myDic[key] = 0;
@@ -22,6 +22,17 @@ public class Solution {
         int maxCount = myDic.Values.Max();
         return myDic.Values.Count(x => x == maxCount);
         }
+}
+[TestFixture]
+public class CountLargestGroupTest
+{
+    [Test]
+    public void Test_n_equals_13()
+    {
+        Solution sol = new();
+        int result = sol.CountLargestGroup(13);
+        Assert.That(result, Is.EqualTo(4));
+    }
 }
 
 
